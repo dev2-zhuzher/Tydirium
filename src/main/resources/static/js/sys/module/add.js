829a9b18b0a1a70f3966f7b5e09412f2b884aa88
yuzhouneng <dev2-zhuzher@outlook.com>
2017-08-18 19:12:23 +0800

    模块管理新增编辑页面代码：输入校验等

---
+++ src/main/resources/static/js/sys/module/add.js
@@ -0,0 +1,35 @@
+﻿/**
+ * 模块管理:新增或修改
+ */
+$(function() {
+	// 加载控件
+	layui .use( [ 'element','form', 'layedit', 'laydate' ], function() {
+		var form = layui.form(), layer = layui.layer, layedit = layui.layedit, laydate = layui.laydate;
+ 
+		// 自定义验证规则
+		form.verify({ 
+			password : [ /(.+){6,12}$/, '密码必须6到12位' ],
+			resource : function(value){
+				
+			},
+			moduleIcon : function(value){
+				var checkRole = $("input[name='icon']").next(".layui-form-radioed");
+				if(checkRole.length == 0){
+					return "显示图标必选";
+				}
+			},
+			checkResource : function(value){
+				var checkRole = $("input[name='resourceIds']").next(".layui-form-checked");
+				if(checkRole.length == 0){
+					return "关联资源必选";
+				}
+			}
+		});
+
+		// 监听提交
+		form.on('submit(moduleAddOrEditForm)', function(data) {
+			 
+		});
+
+	});
+})
