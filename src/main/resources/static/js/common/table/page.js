0c06542ecbc584dbc389fd4c69aa8101a920d0cf
yuzhouneng <dev2-zhuzher@outlook.com>
2017-08-18 18:38:55 +0800

    分页控件调用修改（颜色、页数控制）

--- src/main/resources/static/js/common/table/page.js
+++ src/main/resources/static/js/common/table/page.js
@@ -12,14 +12,14 @@ $(function(){
 
 		laypage({
 			cont : 'laypage',
-			curr: Number($("#page").val()==null ? 1 : $("#page").val()),
+			curr: Number($("#page").val()==null ? 0 : $("#page").val()) + 1,
 			pages : Number($("#pagerTotalPages").val()==null ? 0 : $("#pagerTotalPages").val()),
 			skip : true,
-			skin: '#1E9FFF',
+			skin: '#009688',
 			jump: function(obj, first){
 				//得到了当前页，用于向服务端请求对应数据
 			    var curr = obj.curr;
-			    $("#page").val(curr); // 当前页数
+			    $("#page").val(curr - 1); // 当前页数
 			    $("#size").val(10); // 每页10条
 			    $("#from").submit(); // 提交后台查询 
 			}
