a4740ce53fa03e2460928eb1f2e333aba73f3329
songjia <songjialishan@hotmail.com>
2017-08-22 16:55:25 +0800

    LoginController代码错误提交复原

--- src/main/java/com/vanke/tydirium/web/controller/LoginController.java
+++ src/main/java/com/vanke/tydirium/web/controller/LoginController.java
@@ -17,43 +17,43 @@ import com.vanke.tydirium.service.sys.SysUserService;
  */
 @Controller
 public class LoginController extends BaseController {
-//	/** 鉴权服务地址 */
-//	@Value("${zhuzher_api_host}")
-//	private String zhuzherApiHost;
-//	/** 邮件申请获得的客户端ID */
-//	@Value("${client_id}")
-//	private String clientId;
-//	/** 秘钥 */
-//	@Value("${client_secret}")
-//	private String clientSecret;
-//	/** 跳转地址 */
-//	@Value("${redirect_uri}")
-//	private String redirectUri;
-//	@Value("${response_type}")
-//	private String responseType;
-//	@Value("${scopes}")
-//	private String scopes; 
-//	@Value("${state}")
-//	private String state; 
-//
-//	@Autowired
-//	SysUserService sysUserService;
-//
-//	/**
-//	 * 跳转至登陆页面
-//	 * 
-//	 * @return
-//	 */
-//	@RequestMapping(value = "/login")
-//	public String login(Model model) {
-//		model.addAttribute("zhuzherApiHost", zhuzherApiHost);
-//		model.addAttribute("clientId", clientId);
-//		model.addAttribute("clientSecret", clientSecret);
-//		model.addAttribute("redirectUri", redirectUri);
-//		model.addAttribute("responseType", responseType);
-//		model.addAttribute("scopes", scopes);
-//		model.addAttribute("state", state);
-//		return "login";
-//	}
+	/** 鉴权服务地址 */
+	@Value("${zhuzher_api_host}")
+	private String zhuzherApiHost;
+	/** 邮件申请获得的客户端ID */
+	@Value("${client_id}")
+	private String clientId;
+	/** 秘钥 */
+	@Value("${client_secret}")
+	private String clientSecret;
+	/** 跳转地址 */
+	@Value("${redirect_uri}")
+	private String redirectUri;
+	@Value("${response_type}")
+	private String responseType;
+	@Value("${scopes}")
+	private String scopes; 
+	@Value("${state}")
+	private String state; 
+
+	@Autowired
+	SysUserService sysUserService;
+
+	/**
+	 * 跳转至登陆页面
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/login")
+	public String login(Model model) {
+		model.addAttribute("zhuzherApiHost", zhuzherApiHost);
+		model.addAttribute("clientId", clientId);
+		model.addAttribute("clientSecret", clientSecret);
+		model.addAttribute("redirectUri", redirectUri);
+		model.addAttribute("responseType", responseType);
+		model.addAttribute("scopes", scopes);
+		model.addAttribute("state", state);
+		return "login";
+	}
 
 }
