d562aeac65426608b2020d73285bcd29dd370251
yuzhouneng <dev2-zhuzher@outlook.com>
2017-08-23 18:41:05 +0800

    用户登录：选择项目和角色页面html、js代码

---
+++ src/main/resources/static/js/project.js
@@ -0,0 +1,24 @@
+﻿/**
+ * 选择项目角色登陆页JS
+ */
+
+layui.define([ 'element', 'layer', 'form' ], function(exports) {
+	var form = layui.form();
+	var $ = layui.jquery;
+	// 自定义验证
+	form.verify({
+		checkProjectRole : function(value){
+			var checkRole = $("input[name='roleCode']").next(".layui-form-radioed");
+			if(checkRole.length == 0){
+				return "登陆角色必选";
+			}
+			$("input[name='projectCode']").val(checkRole.prev().attr("projectcode"));
+			$("input[name='description']").val(checkRole.prev().attr("title"));
+		}
+	});
+	// 监听登陆提交
+	form.on('submit(project)', function(data) {
+		
+	}); 
+	exports('project', {});
+});
