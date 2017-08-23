7c9f7ce1d8391d13c452eb517d36d63471aaf93f
songjia <songjialishan@hotmail.com>
2017-08-23 16:11:21 +0800

    冲突解决

---
+++ src/main/java/com/vanke/tydirium/TydiriumApplication.java
@@ -0,0 +1,31 @@
+package com.vanke.tydirium;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 
+ * 
+ * @Description: SpringBoot入口类
+ *
+ * @author: songjia
+ * @date: 2017年8月23日 下午1:59:35
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@MapperScan("com.vanke.tydirium.mapper")
+public class TydiriumApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TydiriumApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TydiriumApplication.class, args);
+	}
+}
